4ee60293068653b2a3becfabf292d95ef5047d29
yova191 <ramosrodriguezy60@gmail.com>
2026-03-26 16:47:47 -0400

    Actualizar Input.tsx 58c996c49c45652a3c521ccfbe57829f10247336

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -10,7 +10,7 @@ export const Input = React.forwardRef<HTMLInputElement, InputProps>(
     return (
       <div className="relative">
         {icon && <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">{icon}</div>}
-        <input
+        <input 58c996c49c45652a3c521ccfbe57829f10247336
           type={type}
           className={`w-full bg-gray-900/50 border border-gray-600 rounded-md shadow-sm placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 text-sm py-2 ${icon ? 'pl-10' : 'px-3'} ${className}`}
           ref={ref}
